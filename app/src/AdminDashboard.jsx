617671e759736edb9f5bae43fd02e3ee537a8fce
x3rud <marco.ribera@erre2web.it>
2025-07-06 14:29:50 +0300

    Fixed status cleaning when refreshing

--- app/src/AdminDashboard.jsx
+++ app/src/AdminDashboard.jsx
@@ -74,7 +74,7 @@ export default function AdminDashboard() {
   }
 
   async function checkStatus(camerasList = cameras) {
-    const statusMap = {};
+    const statusMap = statuses;
     await Promise.all(
       camerasList.map(async (cam) => {
         try {
@@ -113,7 +113,7 @@ export default function AdminDashboard() {
   }
 
   async function checkSingleStatus(cam) {
-    const statusMap = {};
+    const statusMap = statuses;
     try {
       const res = await axios({method: 'get', url: `${API_BASE}/camera-status/${cam.ip}`});
       statusMap[cam.id] = res.data.status;
@@ -309,7 +309,7 @@ export default function AdminDashboard() {
                         
                         <Tooltip>
                           <TooltipTrigger asChild>
-                            <Button onClick={() => sendSingleCommand('put', cam, '/ptz/preset', { operation: "call", id: 0}) }>
+                            <Button onClick={() => sendSingleCommand('put', cam, '/ptz/preset', { operation: "call", id: 0}, false) }>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M9 9V4.5M9 9H4.5M9 9 3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5 5.25 5.25" />
                             </svg>
